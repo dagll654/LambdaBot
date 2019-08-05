d95a825aeba11110c527be97332231e9edcfafa8
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-08-05 20:42:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,5 @@
-const Commando = require('discord.js-commando');
-const sqlite = require('sqlite');
-const client = new Commando.Client({
-	
-    owner: '143261987575562240'
-});
-
-const path = require('path');
-
-client.registry
-    // Registers your custom command groups
-    .registerGroups([
-        ['fun', 'Fun commands'],
-        ['some', 'Some group'],
-        ['other', 'Some other group']
-    ])
-
-    // Registers all built-in groups, commands, and argument types
-    .registerDefaults()
-
-    // Registers all of your commands in the ./commands/ directory
-    .registerCommandsIn(path.join(__dirname, 'commands'));
-
-client.setProvider(
-    sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => new Commando.SQLiteProvider(db))
-).catch(console.error);
+const Discord = require('discord.js');
+ const client = new Discord.Client();
 
  x = 0 
 
