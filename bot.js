6af278bc4c6ed1929538acea3ff49fd01e438cb8
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-08-09 14:55:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
  const client = new Discord.Client();
   const { Client, RichEmbed } = require('discord.js');
-	const DELTAS = client.guilds.get("607318782624399361");
 
+ DELTAS = client.guilds.get("607318782624399361");
+ BCH = DELTAS.channels.get("607558082381217851");
  x = 0 
  x1 = 0
  dbg1 = 0
@@ -10,8 +11,7 @@ const Discord = require('discord.js');
 client.on('ready', () => {
 	
 	// Getting the Lambda's Deltas guild for easy use
-		var BCH = DELTAS.channels.get("607558082381217851");
-		
+
 	// Debug line: logs all members
 	//DELTAS.members.forEach(member => console.log(member.user.username));
 	
