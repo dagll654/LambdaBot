b70218c9ead49134ce26bb8d7f2339b7d0b1b20b
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-11-30 15:24:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -236,7 +236,7 @@ client.on('message', msg => {
 		}  
 		
 		// Emoji command
-		if (cmd[0] === '!em' {
+		if (cmd[0] === '!em') {
 			if (length.emoji(cmd2[1], DELTAS, false, true) === 18) {
 				yeet(0)
 				ch.send(emoji(cmd2[1], DELTAS, false))
