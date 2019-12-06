7f3db826d4efd32cce859e09ed80f5e6cf738c9e
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-06 15:42:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,11 +32,16 @@ client.on('ready', () => {
 	conn.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  conn.query("CREATE DATABASE db_nodejs", function (err, result) {
+  
+  var sql = "CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, fname VARCHAR(30) NOT NULL, lname VARCHAR(30), email VARCHAR(100) NOT NULL, contact VARCHAR(25), note VARCHAR(400))";
+  
+  conn.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Database created successfully");
+    console.log("Table created successfully");
   });
-	})
+});
+	
+	
 	// Getting the Lambda's Deltas guild for easy use
 	const DELTAS = client.guilds.get("607318782624399361");
 	const BCH = DELTAS.channels.get("607558082381217851");
