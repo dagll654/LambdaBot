18274b837c55360eec23249a1b11beda0659c5a8
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-06 12:43:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -221,6 +221,7 @@ client.on('message', msg => {
 	
 	// Function for finding the dep role among a member's roles
 	function drFind(mmbr) {
+		ret = void(0)
 		if (ncdeproles.every(t => msg.member.roles.map(r => r.name).includes(t) === false) === false) {
 		for (i = 0; i <= mmbr.roles.map(r => r.name).length; i++) {
 			if (ncdeproles.includes(mmbr.roles.map(r => r.name)[i])) {
