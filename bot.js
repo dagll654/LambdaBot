7b9b80ac76d7838309cfc581ec2c8e95ab8aa934
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-06 19:42:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,13 +36,13 @@ const Discord = require('discord.js');
  	// Function for finding the dep role among a member's roles
 	function drFind(mmbr) {
 		ret = ""
-		if (ncdeproles.every(t => msg.member.roles.map(r => r.name).includes(t) === false) === false) {
+		if (ncdeproles.every(t => mmbr.roles.map(r => r.name).includes(t) === false) === false) {
 		for (i = 0; i <= mmbr.roles.map(r => r.name).length; i++) {
 			if (ncdeproles.includes(mmbr.roles.map(r => r.name)[i])) {
 				ret = mmbr.roles.map(r => r.name)[i]
 			}
 		}}
-		if (cdeproles.every(t => msg.member.roles.map(r => r.name).includes(t) === false) === false) {
+		if (cdeproles.every(t => mmbr.roles.map(r => r.name).includes(t) === false) === false) {
 		for (i = 0; i <= mmbr.roles.map(r => r.name).length; i++) {
 			if (cdeproles.includes(mmbr.roles.map(r => r.name)[i])) {
 				ret = ncdeproles[cdeproles.indexOf(mmbr.roles.map(r => r.name)[i])]
