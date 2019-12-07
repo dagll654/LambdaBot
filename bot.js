4dba5f5f79ab900f42538a0f462270784563b8b3
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-07 15:56:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -486,8 +486,9 @@ client.on('message', msg => {
 						break
 					}
 					break
+				case "profile":
 					
-					
+					break
 				default:
 					console.log("Unrecognized debug command noticed.")
 					break
@@ -602,7 +603,7 @@ client.on('message', msg => {
 								curruser = dbployees[dbids.indexOf(msg.author.id)]
 								stats = [curruser.fortitude, curruser.prudence, curruser.temperance, curruser.justice]
 								console.log(`F${stats[0]} P${stats[1]} T${stats[2]} J${stats[3]}`)
-								ch.send("\n```mb\n 📋 | Showing stats for user " + curruser.tag + "\n```" + `\n		<:fortitude:652534887332577290>${stats[0]}		<:prudence:652534926503182342> ${stats[1]}\n		<:temperance:652534935583981568> ${stats[2]}		<:justice:652534947378102282> ${stats[3]}`)
+								ch.send("\n```mb\n 📋 | Showing stats for user " + curruser.tag + "\n```" + `\n		LV${Math.ceil(stats[0]/25)} <:fortitude:652534887332577290>${stats[0]}		LV${Math.ceil(stats[1]/25)} <:prudence:652534926503182342> ${stats[1]}		LV${Math.ceil(stats[2]/25)} <:temperance:652534935583981568> ${stats[2]}		LV${Math.ceil(stats[3]/25)} <:justice:652534947378102282> ${stats[3]}`)
 								if (err) throw err
 								connection.release()
 							})
