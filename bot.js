04f1e6a4fe5f91aa050e5fa3d8fd4d803a3546ec
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-07 16:56:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -630,7 +630,7 @@ client.on('message', msg => {
 								dbployees.forEach(e => dbids.push(e.id))
 								stats = [curruser.fortitude, curruser.prudence, curruser.temperance, curruser.justice]
 								console.log(`F${stats[0]} P${stats[1]} T${stats[2]} J${stats[3]}`)
-								ch.send("\n```mb\n 📋 | Showing stats for user " + curruser.tag + "\n```" + `		LV${Math.ceil(stats[0]/25)} <:fortitude:652534887332577290>${stats[0]}		LV${Math.ceil(stats[1]/25)} <:prudence:652534926503182342> ${stats[1]}		LV${Math.ceil(stats[2]/25)} <:temperance:652534935583981568> ${stats[2]}		LV${Math.ceil(stats[3]/25)} <:justice:652534947378102282> ${stats[3]}`)
+								ch.send("\n```mb\n 📋 | Showing stats for user " + curruser.tag + "\n```" + `				LV${Math.ceil(stats[0]/25)} <:fortitude:652534887332577290>${stats[0]}		LV${Math.ceil(stats[1]/25)} <:prudence:652534926503182342> ${stats[1]}		LV${Math.ceil(stats[2]/25)} <:temperance:652534935583981568> ${stats[2]}		LV${Math.ceil(stats[3]/25)} <:justice:652534947378102282> ${stats[3]}`)
 								if (err) throw err
 								connection.release()
 							})
