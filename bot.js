354c4f1265e705e36b5b1f54af5574394fb803fa
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-07 18:06:51 +0300

    upd8

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const Discord = require('discord.js');
   const lambHook = new Discord.WebhookClient(process.env.LAMBDAHOOK_ID, process.env.LAMBDAHOOK_TOKEN);
   const abn = require("./abnb.json");
   const jn = require("./junk.json");
+  const gear = require("./gear.json")
   const animojis = [
 			"restartsForDays",
 			"pepanger",
