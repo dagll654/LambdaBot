5f0154d0f6076811d83ca24632d0dbf2eb947ad1
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-09 19:20:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,6 +78,23 @@ const Discord = require('discord.js');
 	}
  
 client.on('ready', () => {
+	
+	function resolveAfter2Seconds() {
+		return new Promise(resolve => {
+			setTimeout(() => {
+				resolve('resolved')
+			}, 2000)
+		})
+	}	
+
+	async function asyncCall() {
+	  console.log('calling')
+	  var result = await resolveAfter2Seconds()
+	  console.log(result)
+	  // expected output: 'resolved'
+	}
+
+	asyncCall()
 
 
 	// Getting the Lambda's Deltas guild for easy use
