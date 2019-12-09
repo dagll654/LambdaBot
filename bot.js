55d639070c544d7de8d7161f3af349aec91ec430
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-09 19:23:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,19 +79,18 @@ const Discord = require('discord.js');
  
 client.on('ready', () => {
 	
-	function resolveAfter2Seconds() {
+	function wait() {
 		return new Promise(resolve => {
 			setTimeout(() => {
 				resolve('resolved')
-			}, 2000)
+			}, 1000)
 		})
 	}	
 
 	async function asyncCall() {
 	  console.log('calling')
-	  var result = await resolveAfter2Seconds()
-	  console.log(result)
-	  // expected output: 'resolved'
+	  var result = await wait()
+	  console.log("Resolved in a second?")
 	}
 
 	asyncCall()
