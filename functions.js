7bef758295812ddae093595163c9fff87fdb3ab6
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-11 11:50:39 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,12 @@
   const jn = require("./junk.json");
-
+  const gear = require("./gear.json")
+  	const deproles = jn.deproles
+	const ncdeproles = jn.ncdeproles
+	const cdeproles = jn.cdeproles
+	const help1 = jn.help1 
+	const qte = jn.qte
+	const qte2 = "Lambdadelta Quote #"
+	const cmds = jn.cmds
 //module.js
 exports.hello = function() {
   return "Hello";
