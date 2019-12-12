10afbb8168e4141b55ef957de821f40d057367a4
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-12 19:44:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -717,9 +717,12 @@ const Discord = require('discord.js');
 				case "profile":
 				let uid = "" //
 				if (cmd[4] != undefined) {uid = cmd[4]} else {uid = "143261987575562240"}
+					let tempval = cmd[3]
+					if ((cmd[2] === "hp") || (cmd[2] === "sp")) {tempval = Number(cmd[2]).toFixed(1)}
+					
 					let keys = Object.keys(dbployees[dbids.indexOf(uid)])
 					let val = Object.values(dbployees[dbids.indexOf(uid)])
-					dbployees[dbids.indexOf(uid)][cmd[2]] = cmd[3]
+					dbployees[dbids.indexOf(uid)][cmd[2]] = tempval
 					updData()
 					break
 				default:
