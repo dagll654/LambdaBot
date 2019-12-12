bbcc17e62c0f0e9980797af4926951bcb52bd9a9
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-12 21:54:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -704,7 +704,7 @@ const Discord = require('discord.js');
 					bumpBoxes(Number(cmd[2]), cmd[3], cmd[4])
 					break
 				case "crash":
-					client.destroy()
+					client.destroy(process.env.BOT_TOKEN)
 					break
 				case "var":
 					console.log("Debug command !debug var noticed.")
@@ -924,15 +924,18 @@ const Discord = require('discord.js');
 				//			break
 				//	}
 				//progressBarStorage.push(progressBar)
-				} else {curruser.dead = 1}
+				} else {dbployees[dbids.indexOf(msg.author.id)].dead = 1}
 				}
 				
 				async function asyncEdit(mssage) {
+					
 					if (damageArray.length === 0) {damageArray.push("none")}
 						let wtime = Math.floor((currentAbno.peoutput/2)*10)/10
 						mssage.edit("\n```mb\n ⚙️ | User " + curruser.tag + " is working " + cmd[3] + " on " + currentAbno.name + "\n```" + `	Currently working, this will take approximately ${wtime} seconds.`)
 						await wait(wtime*500)
 						//console.log("ARR length: " + arr.length)
+						if ((Number(curruser.hp).toFixed(1) === "0.0") || (Number(curruser.sp).toFixed(1) === "0.0"))
+						{curruser.dead = 1; dbployees[dbids.indexOf(msg.author.id)].dead = 1}
 						if (curruser.dead === 0) {
 						ppe = ""
 						if (ppeboxes > 0) {ppe = `\n	Pure (wild card) PE boxes: ${ppeboxes}`}
@@ -965,6 +968,7 @@ const Discord = require('discord.js');
 						curruser = dbployees[dbids.indexOf(cuid)]//
 					} else {curruser = dbployees[dbids.indexOf(msg.author.id)]}
 								//console.log("Curruser ID (profile): " + curruser.id)
+								let deathArr = ["alive", "dead"] 
 								let ssp = bumpSubpoint(curruser.id)
 								eqct = [curruser.suit, curruser.weapon]
 								// [Suit, Weapon]
@@ -975,7 +979,7 @@ const Discord = require('discord.js');
 								for (i = 0; i < 4; i++) {
 									if (gearc[1].dtype[i] > 0) {wepd += jn.dtype[i]}
 								}
-								ch.send("\n```mb\n 📋 | Showing stats for user " + curruser.tag + "\n```" + `		LV ${statLVL(stats[0])} ${jn.fortitude} ${stats[0]}			LV ${statLVL(stats[1])} ${jn.prudence} ${stats[1]}\n		LV ${statLVL(stats[2])} ${jn.temperance} ${stats[2]}			LV ${statLVL(stats[3])} ${jn.justice} ${stats[3]}\nProgress towards the next stat points:\n		${jn.fortitude} ${ssp[0]} / ${(jn.statLevels.indexOf(statLVL(stats[0]))+1)*16}		${jn.prudence} ${ssp[1]} / ${(jn.statLevels.indexOf(statLVL(stats[1]))+1)*16}\n		${jn.temperance} ${ssp[2]} / ${(jn.statLevels.indexOf(statLVL(stats[2]))+1)*16}		${jn.justice} ${ssp[3]} / ${(jn.statLevels.indexOf(statLVL(stats[3]))+1)*16}\n\n		HP: ${Number(curruser.hp).toFixed(1)}${jn.health}		SP: ${Number(curruser.sp).toFixed(1)}${jn.sanity}\n\n		Suit: ${gearc[0].name}   -   ${gearc[0].resistance[0]} ${jn.dtype[0]}	${gearc[0].resistance[1]} ${jn.dtype[1]}	${gearc[0].resistance[2]} ${jn.dtype[2]}	${gearc[0].resistance[3]} ${jn.dtype[3]}\n		Weapon: ${gearc[1].name}   -   ${wepd}`)
+								ch.send("\n```mb\n 📋 | Showing stats for user " + curruser.tag + "\n```" + `		LV ${statLVL(stats[0])} ${jn.fortitude} ${stats[0]}			LV ${statLVL(stats[1])} ${jn.prudence} ${stats[1]}\n		LV ${statLVL(stats[2])} ${jn.temperance} ${stats[2]}			LV ${statLVL(stats[3])} ${jn.justice} ${stats[3]}\nProgress towards the next stat points:\n		${jn.fortitude} ${ssp[0]} / ${(jn.statLevels.indexOf(statLVL(stats[0]))+1)*16}		${jn.prudence} ${ssp[1]} / ${(jn.statLevels.indexOf(statLVL(stats[1]))+1)*16}\n		${jn.temperance} ${ssp[2]} / ${(jn.statLevels.indexOf(statLVL(stats[2]))+1)*16}		${jn.justice} ${ssp[3]} / ${(jn.statLevels.indexOf(statLVL(stats[3]))+1)*16}\n\n		Currently:	${deathArr[number(curruser.dead)]}.\n		HP: ${Number(curruser.hp).toFixed(1)}${jn.health}		SP: ${Number(curruser.sp).toFixed(1)}${jn.sanity}\n\n		Suit: ${gearc[0].name}   -   ${gearc[0].resistance[0]} ${jn.dtype[0]}	${gearc[0].resistance[1]} ${jn.dtype[1]}	${gearc[0].resistance[2]} ${jn.dtype[2]}	${gearc[0].resistance[3]} ${jn.dtype[3]}\n		Weapon: ${gearc[1].name}   -   ${wepd}`)
 								if (err) throw err
 					break
 				case "i":
