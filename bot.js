fdac1b9b96e516517e4e86cd063d7ff14b5555c5
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-12 06:59:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -227,7 +227,7 @@ function databaseThing() {
 				bAbnos.forEach(a => {
 					bToSend.push(a + "|" + bBals[bAbnos.indexOf(a)])
 				})
-				console.log("LOOK AT MY BALLS " + bToSend.join(" "))}
+				console.log("LOOK AT MY BALLS " + bToSend.join(" "))
 				
 				connection.query("UPDATE `employees` SET `balancespecific` = '" + bToSend.join(" ") + "' WHERE `employees`.`userid` = '" + e.id + "';", function (err, result) {if (err) throw err})	
 			})
