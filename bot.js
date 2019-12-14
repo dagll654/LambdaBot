fc987324bb3f0cba9e71f21d1774c4e2ef1259a6
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-14 18:57:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -335,11 +335,11 @@ const Discord = require('discord.js');
 	})
 
 	client.on('message', tempbigmessagevaluesoIneveruseitagain => {
-	
+		
+	const DELTAS = client.guilds.get("607318782624399361");
+	const bch = DELTAS.channels.get("607558082381217851");
 	const msg = tempbigmessagevaluesoIneveruseitagain
-	
 	const ESERV = client.guilds.get('513660754633949208')
-
 	const bsch = ESERV.channels.get('653572131262693379')
 
 	var emojiid = DELTAS.emojis.map(e => e.id)
