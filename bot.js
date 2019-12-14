f9f48351ff3b8f30a75ed0337e4a807b556e7db1
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-14 19:05:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -830,8 +830,8 @@ const Discord = require('discord.js');
 					ch.send({embed})
 					break
 				case "employee":
-					uid = ""
-					if (cmd[2] != undefined) {uid = cmd[2]} else {uid = "143261987575562240"}
+					let uid2 = ""
+					if (cmd[2] != undefined) {uid2 = cmd[2]} else {uid = "143261987575562240"}
 					console.log(dbployees[dbids.indexOf(uid)])
 					break
 				case "msg":
