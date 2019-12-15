9b4b570c7e6cbefcb6347a440ace9e7dd306a88d
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-16 00:41:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -582,7 +582,7 @@ const Discord = require('discord.js');
 					if (abn.abn[abn.lista.indexOf(arrg[1])].dtype[3] === 1) {
 						dmg = dmg * rDamage(gear.suits[Number(dbployees[dbids.indexOf(arrg[0])].suit)].level, abn.abn[abn.lista.indexOf(arrg[1])].risk, gear.suits[Number(dbployees[dbids.indexOf(arrg[0])].suit)].resistance[2]*dbployees[dbids.indexOf(arrg[0])].defensebuffs.split("|")[2])
 						damageArray.push(dmg.toFixed(2) + " " + jn.dtype[3])
-						dbployees[dbids.indexOf(arrg[0])].hp -= (dbployees[dbids.indexOf(arrg[0])].hp/100)*dmg
+						dbployees[dbids.indexOf(arrg[0])].hp -= (dbployees[dbids.indexOf(arrg[0])].fortL/100)*dmg
 						//console.log("DAMAGE:" + dmg)
 					}
 					
