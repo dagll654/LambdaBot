3fbb4afd9166a3c47ec828babac92834fc77b961
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-20 20:08:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1563,7 +1563,6 @@ const Discord = require('discord.js');
 									
 									// Main menu of extraction 
 									case "main": 
-									menumsg.edit("\n```mb\n 📤 | Welcome to the extraction hub, employee " + cUser.tag + ".\n```\n" + `	Please input the code of the abnormality, EGO equipment of which you wish to extract.`)
 									if (jn.abnWorkable.includes(rp.content.toLowerCase())) {
 										currentAbno = abn.abn[abn.lista.indexOf(rp.content.toLowerCase())]
 										currentAbnoCode = rp.content.toLowerCase()
@@ -1622,6 +1621,8 @@ const Discord = require('discord.js');
 											cUser.balance -= prices[1]
 											addItemID(cUser, cInv, currentAbno.ego)
 											menuIndex = "shop"
+											menumsg.edit("\n```mb\n 📤 | Welcome to the extraction hub, employee " + cUser.tag + ".\n```\n" + `	Please input the code of the abnormality, EGO equipment of which you wish to extract.`)
+											k = 1
 											break
 										}
 										else if (rp.content.toLowerCase() === "n") {forceReturn(rp, "purchase cancelled."); menuIndex = "shop"; k = 1; break}
