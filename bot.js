c082cfe306021bb1025afaac9f549b89bcb5f9c2
dagll654 <53710533+dagll654@users.noreply.github.com>
2019-12-21 15:54:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -274,7 +274,7 @@ const Discord = require('discord.js');
 			console.log("To push:")
 			console.log(dbpush)
 			dbpush.forEach(e => {
-			var sql = "INSERT INTO employees (userid, usertag, balancespecific, hp, sp) VALUES ('" + e.id + "', '" + e.tag + "', '0|0|0|0', '17000', '17000')";
+			var sql = "INSERT INTO employees (userid, usertag, balancespecific, hp, sp) VALUES ('" + e.id + "', '" + e.tag + "', '0|0', '1700', '1700')";
 			connection.query(sql, function (err, result) {
 			if (err) throw err;
 			console.log(`${e.tag} inserted!`)
@@ -1500,11 +1500,15 @@ const Discord = require('discord.js');
 						var rtmp = cmd[2]
 						if (jn.nccideproles.includes(rtmp)) {
 							msg.member.addRole(getRole(ncdeproles[jn.nccideproles.indexOf(rtmp)]))
+							console.log("test: 1")
 							if (dbids.includes(msg.author.id) === false) {
+								console.log("test: false")
 								dbids.push(msg.author.id)
 								dbployees.push({"id": msg.author.id, "tag": msg.author.tag, "hp": 17, "sp": 17, "fortitude": 17, get fortL() {return (Number(this.fortitude)+Number(this.buffs.split("|")[0]))}, "prudence": 17, get prudL() {return (Number(this.prudence)+Number(this.buffs.split("|")[1]))}, "temperance": 17, get tempL() {return (Number(this.temperance)+Number(this.buffs.split("|")[2]))}, "justice": 17, get justL() {return (Number(this.justice)+Number(this.buffs.split("|")[3]))}, "suit": 0, "weapon": 0, "inventorys": undefined, "inventoryw": undefined, "gifts": undefined, "working": 0, "dead": 0, "balance": 0, "balancespecific": '', "subpoints": "0|0|0|0", "effects": "null", "buffs": "0|0|0|0", "defensebuffs": "1|1|1|1", "bufflist": undefined, "tjtime": Date.now(), "statlimit": 100, get stats() {return [Number(this.fortitude), Number(this.prudence), Number(this.temperance), Number(this.justice), statLVN(this.fortitude)+statLVN(this.prudence)+statLVN(this.temperance)+statLVN(this.justice)]}})
 							}
+							console.log("test: 2")
 							msg.reply("you have been successfully assigned to work in the " + ncdeproles[jn.nccideproles.indexOf(rtmp)] + "!")
+							databaseThing()
 							databaseThing()
 						} else {msg.reply("error: incorrect team name. Example: !lc assign extraction team")}
 					} else {msg.reply("you can only work in one team at a time. Leave your team (!lc leave) if you want to join another team.")}
