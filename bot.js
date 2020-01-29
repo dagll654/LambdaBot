3aa160afd1eeefb5bfe8b181144ca8f1dcd832b7
dagll654 <53710533+dagll654@users.noreply.github.com>
2020-01-29 14:08:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1707,7 +1707,7 @@ const Discord = require('discord.js');
 						msg.reply("To get assigned to a team, type in !lc assign (Team name).")
 						
 					} else {
-						helparr = [
+						let helpArr = [
 							"Disambiguation: arguments in [square brackets] are optional, arguments in (parentheses) are required for the command to work.\n",
 							"	`!lc p [employee's nickname/discord tag]` - shows your profile if no ",
 							"	`!lc w (abnormality ID) (work order)` - executes the selected work order (instinct, insight, attachment or repression) on the abnormality with the specified ID. Use `!lc w list` to see the list of all abnormalities currently in the facility.",
@@ -1719,7 +1719,7 @@ const Discord = require('discord.js');
 							"		`!lc captain vote (@employee)` - initiates a vote for the mentioned employee to become the captain of your department, if one is not assigned already.",
 							"		`!lc captain resign` - (captain command) initiates the procedure of captain role resignation. *Does* have a confirmation message."
 						]
-						msg.reply("`	!lc p` - shows your profile \n	")
+						msg.reply(helpArr.join("\n"))
 						}
 					
 					break
