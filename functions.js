fffeda6c2ea465997f873e824cc766935c59cfaf
dagll654 <53710533+dagll654@users.noreply.github.com>
2020-01-31 19:40:48 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -160,7 +160,7 @@ exports.gift = function(employee, abnoID, result) {
 		
 		default: return [false, 2]
 	}
-	
+	if (employee.luck > 1000) gRRes = [true, 0]
 	if (gRRes[0] === true) {
 	let gift = gear.gifts.find(g => g.id === abnoID)
 	let gifts = employee.gifts.split("|")
