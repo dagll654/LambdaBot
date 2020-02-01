30ea7f90545fb711594bfc5bbaed6bb700cff85d
dagll654 <53710533+dagll654@users.noreply.github.com>
2020-02-01 19:32:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1174,7 +1174,7 @@ const Discord = require('discord.js')
 				let mem = DELTAS.members.get(getUser(cmd[1]).id)
 				let backr = []
 				roles.forEach(r => {
-					if (mem.roles.find(r) === undefined) backr.push(r)
+					if (mem.roles.find(mr => mr.id === r.id) === undefined) backr.push(r)
 				})
 				if (backr != []) mem.addRoles(backr)
 								.catch(console.error)
