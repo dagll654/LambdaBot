72a631ab6f0fe39f90c473e42c26ab0ecbbdd405
dagll654 <53710533+dagll654@users.noreply.github.com>
2020-02-06 22:48:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,7 +83,7 @@ function drFind(member) {
 }
 
 // Employee class
-class cEmp {
+let cEmp = class {
 	constructor(id, tag, hp = 1700, sp = 1700, fortitude = 17, prudence = 17, temperance = 17, justice = 17, suit = "0", weapon = "0", inventorys, inventoryw, working = 0, dead = 0, balance = 0, balancespecific = "", subpoints = "0|0|0|0", effects = 'null', buffs = "0|0|0|0", defensebuffs = "1|1|1|1", bufflist, tjtime = Date.now(), statlimit = 100, gifts = "", inventory = "", luck = 0) {
 		this.id = id
 		this.tag = tag
