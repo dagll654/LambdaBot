3b96620b7660ea2f188e909de65da836bbbef2c4
dagll654 <53710533+dagll654@users.noreply.github.com>
2020-02-08 00:19:49 +0300

    upd9

--- bot.js
+++ bot.js
@@ -516,6 +516,7 @@ function work(employee1, abno1, order1, channel) {
 		}
 		if (damageArray.length === 0) damageArray.push("none")
 		let wTime = Math.floor((cAbno.peoutput/2)*10)/10
+		if (cAbno.code === "o-01-01") wTime = 20
 		rMsg.edit("\n```mb\n ⚙️ | Employee " + e.tag + " is working " + order + " on " + cAbno.name + "\n```" + `	Currently working, this will take approximately ${wTime} seconds.`)
 		await wait(wTime*500)
 		if (Number(e.hp) <= 0 || Number(e.sp) <= 0)
