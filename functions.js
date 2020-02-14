dd78780a6651e3587e5de901a57f703b96047731
dagll654 <53710533+dagll654@users.noreply.github.com>
2020-02-14 10:31:48 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -428,12 +428,9 @@ exports.effectApplication = {
 		let effects = employee.effectArray
 		if (effects.some(e => e[0] === "3")) {
 			let fatigue = effects.find(e => e[0] === "3")
-			console.log(`Fatigue (f): ${effects.find(e => e[0] === "3")}`)
-			let riskMod = employee.stats[4] - risk + 1
+			let riskMod = employee.stats[4] - jn.risk.indexOf(risk) + 1
 			fatigue[3] = Number(fatigue[3]) + riskMod
 			fatigue[1] = 40 + Number(fatigue[3])
-			console.log(`Fatigue (l): ${fatigue}`)
-			console.log(`Fatigue (f): ${effects.find(e => e[0] === "3")}`)
 		} else effects.push([3, 40, "Fatigue", 0])
 		employee.effects = effects.map(e => e.join("/")).join("|")
 	},
