f54e97e0a3b93066b0f1e813dc31e0da15dfe30d
dagll654 <53710533+dagll654@users.noreply.github.com>
2020-02-14 10:50:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -593,6 +593,7 @@ function globalEffectTick() {
 		e.effects = effectsNew.map(eff => eff.join("/")).join("|") // Wraps the effects array up neatly and puts them on the employee
 		} else e.effects = 'null'
 		if (exists(e.effects) === false && e.effects != "null") e.effects = "null"
+		else console.log(`${e.tag}: ${effectsNew}`)
 	}
 	})
 	if (efflog >= debugVariables["effect_log"]) efflog = 0
