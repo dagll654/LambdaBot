7b45828860376e35008a14906ee45515580e7ea8
dagll654 <53710533+dagll654@users.noreply.github.com>
2020-02-17 10:55:13 +0300

    Update simpleCombat.js

--- simpleCombat.js
+++ simpleCombat.js
@@ -1,6 +1,7 @@
-const jn = require("./junk.json")
-const gear = require("./gear.json")
-const fn = require("./functions.js")
+const abn = require("./abnb.json") // Abnormalities
+const jn = require("./junk.json") // Miscellaneous stuff
+const gear = require("./gear.json") // Gear and items
+const fn = require("./functions.js") // Functions, like all effects - on-consume of consumables, on-work of abnos etc.
 const main = require("./bot.js")
 Number.prototype.shortFixed = function(length) {return Math.round(this*Math.pow(10, length))/Math.pow(10, length)}
 Array.prototype.ids = function() {return this.map(e => e.id)}
