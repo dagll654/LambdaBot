742c79458952cf5eca89acb41583113a40a41a0e
dagll654 <53710533+dagll654@users.noreply.github.com>
2020-03-22 17:05:45 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -230,6 +230,7 @@ exports.gift = function(employee, abnoID, result) {
 exports.effects = {
 	"deathOnWork": function(employee, abn) {
 		let ret = false
+		if exists(employee.effects)
 		if (employee.effects.length > 0) {
 			let effectArray = employee.effects.split("|")
 			let deathEffect = []
