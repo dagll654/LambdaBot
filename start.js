b1daba71fa28836f3960c1fda83ae29b73e168d2
dagll654 <53710533+dagll654@users.noreply.github.com>
2020-08-02 14:12:35 +0300

    Delete start.js

--- start.js
+++
@@ -1,9 +0,0 @@
-const nodemon = require('nodemon')
-nodemon({ script: 'bot.js', ext: 'js json' })
-nodemon.on('start', function () {
-  console.log('App has started')
-})
-
-nodemon.on('message', message => {
-	nodemon.emit(message)
-})
