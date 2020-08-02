d61f46b0471c89f5a1a7f6c59f3d44a7c81375a9
dagll654 <53710533+dagll654@users.noreply.github.com>
2020-08-02 13:58:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,13 +17,36 @@ input: process.stdin,
 output: process.stdout
 })
 // Setting up the connection pool. Not sure if this is better than just a db.createConnection (or something like that), but I doubt it really matters
-var connection  = db.createConnection({
+var db_config  = {
     host: "lacreme2.heliohost.org",
 	user: "lacreme2_bot",
 	password: process.env.DB_PASS,
 	database: "lacreme2_bot"
-})
-connection.connect(function(err2) {
+}
+
+var connection
+
+function handleDisconnect() {
+  connection = db.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err2) {              // The server is either down
+    if(err2) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
 connection.query(`SHOW PROCESSLIST;`, (err, result) => {
 	if (err) throw err
 	deadConnections = result.filter(c => c.Command === "Sleep")
@@ -2690,4 +2713,4 @@ if ((mesc.toLowerCase().split(" ").indexOf('uwu') > -1 ) || (mesc.toLowerCase().
 // NO TOUCHING
 //______________________________\\/
 client.login(process.env.BOT_TOKEN)
-}) // End 
+//}) // End 
